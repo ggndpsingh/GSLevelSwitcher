0b22457abcd41c8b1c897558952bdf34cbfce15d
Gagandeep Singh <gagan.deep@me.com>
2018-08-28 21:22:11 +1000

    Initial Commit

---
+++ GSLevelSwitcher/GSLevelSwitcher.h
@@ -0,0 +1,19 @@
+//
+//  GSLevelSwitcher.h
+//  GSLevelSwitcher
+//
+//  Created by Gagandeep Singh on 28/8/18.
+//  Copyright © 2018 Gagandeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSLevelSwitcher.
+FOUNDATION_EXPORT double GSLevelSwitcherVersionNumber;
+
+//! Project version string for GSLevelSwitcher.
+FOUNDATION_EXPORT const unsigned char GSLevelSwitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSLevelSwitcher/PublicHeader.h>
+
+
